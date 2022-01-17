c06329dd27de2ca1319abcd427d877cddb9f7110
lcoinsin <coinsin.l@outlook.com>
2022-01-17 22:41:32 +0100

    add requires maven jackson core

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,8 @@ module com.kikichante.kikichantefx {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires javafx.media;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
     opens com.kikichante.kikichantefx to javafx.fxml;
     exports com.kikichante.kikichantefx;
