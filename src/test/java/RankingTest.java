1fceb5a08d8764b2b21056700067647cbbf38487
lcoinsin <coinsin.l@outlook.com>
2022-01-24 23:43:08 +0100

    Ranking

---
+++ src/test/java/RankingTest.java
@@ -0,0 +1,9 @@
+import com.kikichante.controller.ControllerRanking;
+
+public class RankingTest {
+    public static void main(String[] args) {
+        ControllerRanking controllerRanking = new ControllerRanking();
+        String message= "SCORESCREEN:mat-1:le-6:mateo-1:tib-3";
+        controllerRanking.printScoreFx(message);
+    }
+}
